acb40ea8974042a6f5737c23c519c53fbbe13a42
Ulisses Douglas <ulisses.portela@hmail.com>
2020-09-04 17:11:46 -0300

    Separando o footer com um componente

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule { }
