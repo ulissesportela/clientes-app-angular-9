ec262c1a0acb2de1c60a9efc612423d3ad290585
Ulisses Douglas <ulisses.portela@hmail.com>
2020-09-04 17:40:34 -0300

    configurando as rotas e criando o menu

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [NavbarComponent, SidebarComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent,
